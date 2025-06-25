76b5184554fce2acfdd8de3b9c8a217c4862ab33
E-ugine <agollaeugine@gmail.com>
2025-06-25 13:03:05 +0300

    chore:connect to React App

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],  # React dev servers
+    allow_origins=["http://localhost:8080", "http://localhost:5173"],  # React dev servers
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
