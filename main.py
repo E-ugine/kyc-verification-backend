4592a79f7a039a2f45460b5cd20ae3674100afd0
E-ugine <agollaeugine@gmail.com>
2025-06-28 16:00:02 +0300

    fix:redundant packages

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
-from database import engine
-import models
-from routes import kyc, admin
+from app.database import engine
+from app import models
+from app.routes import kyc, admin
 import uvicorn
 import os
 
